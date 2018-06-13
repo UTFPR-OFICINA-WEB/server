9f6d775b8a1d057746b6417b75915a1343903658
André Bonavigo Jakubiu <abjandre@gmail.com>
2018-06-13 20:55:24 -0300

    terminado

--- src/main/java/br/edu/utfpr/pb/oficinaweb/model/Fornecedor.java
+++ src/main/java/br/edu/utfpr/pb/oficinaweb/model/Fornecedor.java
@@ -5,16 +5,12 @@ import java.io.Serializable;
 import javax.persistence.Entity;
 import javax.persistence.PrimaryKeyJoinColumn;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
 
 @Entity
 @Data
 @EqualsAndHashCode(callSuper=true)
-/*@NoArgsConstructor
-@AllArgsConstructor*/
 @PrimaryKeyJoinColumn(referencedColumnName="id")
 public class Fornecedor extends Pessoa implements Serializable{
 
